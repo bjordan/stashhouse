49411c75e5c4deae039e402c74956b37d25aef1f
Barry Jordan <jordanbarry@gmail.com>
2012-02-13 13:11:15 +0000

    Added helper to scoreboard spec

--- spec/scoreboard_spec.rb
+++ spec/scoreboard_spec.rb
@@ -10,39 +10,35 @@ module StashHouse
       ScoreBoard.any_instance.stub(:write_to_file).and_return()
     end
 
+    # helper method
+    def create_and_update(name) do
+      p = Playa.new(name)
+      p.moves = 5
+      s = ScoreBoard.new
+      s.update(p)      
+    end
+
     describe "#update" do
       context "player not already on scoreboard" do
         it "adds player and score" do
-          p = Playa.new("Henry")
-          p.moves = 5
-          s = ScoreBoard.new
-          s.update(p)
+          create_and_update("Henry")
           @scoreboard["Henry"].should eql 5
         end
 
         it "now has one more entry" do
-          p = Playa.new("Henry")
-          p.moves = 5
-          s = ScoreBoard.new
-          s.update(p)
+          create_and_update("Henry")
           @scoreboard.length.should eql 3
         end
       end
 
       context "player already on scoreboard" do
         it "updates existing score" do
-          p = Playa.new("Barry")
-          p.moves = 5
-          s = ScoreBoard.new
-          s.update(p)
+          create_and_update("Barry")
           @scoreboard["Barry"].should eql 5
         end
 
         it "scoreboard has same number of entries" do
-          p = Playa.new("Barry")
-          p.moves = 5
-          s = ScoreBoard.new
-          s.update(p)
+          create_and_update("Barry")
           @scoreboard.length.should eql 2
         end
       end
